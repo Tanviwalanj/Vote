71be566c7aae2e3c184eb2580767af11ffaa25ba
Tanviwalanj <walat029@newschool.edu>
2023-04-22 00:51:34 -0400

    button testing

--- Dayofpoll/script.js
+++ Dayofpoll/script.js
@@ -1,20 +1,9 @@
-const resizeSwatch1 = document.querySelector('.resizeSwatch1')
-const radioContainer1 = document.querySelector('#radioContainer_1')
+// const resizeSwatch1 = document.querySelector('.resizeSwatch1')
+// const radioContainer1 = document.querySelector('#radioContainer_1')
 
-resizeSwatch1.onclick = () =>{
-  // radioContainer1.style.backgroundColor = 'red'
-  radioContainer1.classList.add('red')
-  radioContainer1.classList.remove('red')
+// resizeSwatch1.onclick = () =>{
+//   // radioContainer1.style.backgroundColor = 'red'
+//   radioContainer1.classList.add('red')
+//   radioContainer1.classList.remove('red')
   
-}
-
-
-
-
-// function myFunction() {
-//     document.getElementById("radioContainer_1").innerHTML = "Helloo";
 // }
-
-// <button type="button" onclick="myFunction()"></button>
-
-
